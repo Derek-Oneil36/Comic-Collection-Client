b92410c15cde622850f59d578701b2408692d464
LightCloud36 <dereko.neil@yahoo.com>
2018-10-19 16:12:39 -0400

    Added the correct production path

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://polar-tor-15749.herokuapp.com/',
+  production: 'https://dry-fortress-76779.herokuapp.com/',
   development: 'http://localhost:4741/'
 }
 
